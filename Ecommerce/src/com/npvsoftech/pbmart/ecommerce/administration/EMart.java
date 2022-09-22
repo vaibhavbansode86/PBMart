c16232c13ca0b98fb6f20c46c2f01044919cb29c
nadeem6236 <ernadeem008@gmail.com>
2022-09-22 14:20:35 +0530

    trail conflict

--- Ecommerce/src/com/npvsoftech/pbmart/ecommerce/administration/EMart.java
+++ Ecommerce/src/com/npvsoftech/pbmart/ecommerce/administration/EMart.java
@@ -7,11 +7,9 @@ import com.npvsoftech.pbmart.ecommerce.registration.CustomerManagement;
 
 public class EMart {
 	public static Scanner sc = new Scanner(System.in);
-<<<<<<< HEAD
-   // heading
-=======
-   
->>>>>>> NADEEM
+
+
+
 	static {
 
 		System.out.println(StringUtils.center(StringUtils.center("Welcome To PBMart", 130 - 112), 130, "*"));
